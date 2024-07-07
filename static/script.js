848054cc1ee87c4bc83369efb4c011f2ca2ee802
Priyanshu <priyanshuhoney2002@gmail.com>
2024-07-07 22:14:01 +0530

    Back Camera and Help section

--- static/script.js
+++ static/script.js
@@ -2,13 +2,25 @@
 function startCamera() {
     const video = document.getElementById('camera-stream');
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-            video.srcObject = stream;
-            video.play();
-        });
+        // Specify constraints to select the camera
+        const constraints = {
+            video: {
+                facingMode: 'environment'  // 'user' for front camera, 'environment' for back camera
+            }
+        };
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(function(stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function(error) {
+                console.error('Error accessing the camera:', error);
+            });
     }
 }
 
 window.onload = function() {
     startCamera();
-}
+};
+
